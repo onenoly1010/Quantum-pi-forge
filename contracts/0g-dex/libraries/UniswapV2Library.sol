9aca37e1a689604745be5009f3064a552f3f7bc4
Kris Olofson <onenoly1010@gmail.com>
2026-04-14 20:53:20 -0600

    refactor(0g-dex): Upgrade Solidity to 0.8.24 & use dynamic pair init code hash
    * Bump solidity pragma from old pinned versions to ^0.8.24 on DEX contracts
    * Replace hardcoded pair init hash with dynamically computed on-chain value
    * Expose public INIT_CODE_PAIR_HASH constant on Factory for verification
    * Fix address type casting to comply with Solidity 0.8 type safety requirements

--- contracts/0g-dex/libraries/UniswapV2Library.sol
+++ contracts/0g-dex/libraries/UniswapV2Library.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity =0.6.6;
+pragma solidity ^0.8.24;
 
+import '../UniswapV2Pair.sol';
 import '../interfaces/IUniswapV2Pair.sol';
-
 import "./SafeMath.sol";
 
 library UniswapV2Library {
@@ -18,11 +18,11 @@ library UniswapV2Library {
     // calculates the CREATE2 address for a pair without making any external calls
     function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
         (address token0, address token1) = sortTokens(tokenA, tokenB);
-        pair = address(uint(keccak256(abi.encodePacked(
+        pair = address(uint160(uint(keccak256(abi.encodePacked(
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                keccak256(type(UniswapV2Pair).creationCode) // Path A: Dynamically computed hash
             ))));
     }
 
